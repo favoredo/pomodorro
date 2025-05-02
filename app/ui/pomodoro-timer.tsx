cb04d7fc2d339550533c8d14573e36a7e8795bf0
Aleksandr Fedorov <a.fedorov.work@gmail.com>
2025-05-02 19:05:38 +0300

    init

---
+++ app/ui/pomodoro-timer.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function PomodoroTimer() {
+    return (
+        <Image
+            src="/pomodoro.svg"
+            width={378}
+            height={378}
+            alt="pomodoro image not found"
+        />
+    )
+}
