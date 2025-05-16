1f43537b9fa0b5499f072682afa9a782959b7865
Aleksandr Fedorov <a.fedorov.work@gmail.com>
2025-05-16 21:02:45 +0300

    update paths

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  output: "export", // Enables static export
+  basePath: process.env.NODE_ENV === "production" ? "/pomodorro" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/pomodorro/" : ""
 };
 
 export default nextConfig;
