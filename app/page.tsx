0e03802eb2d72473a93ed971c6cd73e50e420ada
Aleksandr Fedorov <a.fedorov.work@gmail.com>
2025-05-25 20:38:15 +0300

    another ym

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import PomodoroTimer from '@/app/ui/pomodoro-timer'
-import YandexMetrika from '@/app/ui/yandex-metrika'
+import { YandexMetrika } from '@/app/ui/YandexMetrika';
+import Head from 'next/head';
+import { Suspense } from 'react';
+
 
 export default function Page() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <YandexMetrika />
+      <Head>
+        <Suspense>
+          <YandexMetrika />
+        </Suspense>
+      </Head>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start justify-center w-[min(50vw,50vh)] h-[min(50vw,50vh)]">
         <PomodoroTimer />
       </main>
--- app/ui/yandex-metrika.tsx
+++
@@ -1,9 +0,0 @@
-import { YMInitializer } from 'react-yandex-metrika';
-
-export default function YandexMetrika() {
-  return (
-    <div>
-        <YMInitializer accounts={[102157439]} />
-    </div>
-  );
-}
